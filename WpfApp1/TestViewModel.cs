10c0d0ea87416a44d7cb6588411c404df893246d
Nathan Bierema <nbierema@gmail.com>
2022-05-25 21:57:23 -0400

    Add initial code

---
+++ WpfApp1/TestViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    internal class TestViewModel
+    {
+        public ICommand TestCommand { get; }
+
+        public TestViewModel()
+        {
+            TestCommand = new RelayCommand(TestMethod);
+        }
+
+        public void TestMethod(object parameter)
+        {
+            MessageBox.Show("Test");
+        }
+    }
+}
